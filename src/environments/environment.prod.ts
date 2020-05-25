c1c14d7ccc9c2bc5b085a3fc5ec05dcbd3b43220
alee_2695@live.com.ar <alee_2695@live.com.ar>
2020-05-25 20:47:52 -0300

    enlazado con firebase y cordova, test conexion db OK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAm-LjISBlEKc_CPlobCKUM2lky-FJEP3o",
+    authDomain: "lacomanda-pps.firebaseapp.com",
+    databaseURL: "https://lacomanda-pps.firebaseio.com",
+    projectId: "lacomanda-pps",
+    storageBucket: "lacomanda-pps.appspot.com",
+    messagingSenderId: "48076322712",
+    appId: "1:48076322712:web:9e07ac73135f63883a1ded"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyAm-LjISBlEKc_CPlobCKUM2lky-FJEP3o',
+    authDomain: 'lacomanda-pps.firebaseapp.com',
+    databaseURL: 'https://lacomanda-pps.firebaseio.com',
+    projectId: 'lacomanda-pps',
+    storageBucket: 'lacomanda-pps.appspot.com',
+    messagingSenderId: '48076322712',
+    appId: '1:48076322712:web:9e07ac73135f63883a1ded'
+  }
 };
 
 /*
